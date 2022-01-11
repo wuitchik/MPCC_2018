da6fc5851c629c1bef48288fd103619c9dd5203b
Daniel Wuitchik <Daniel.Wuitchik@gmail.com>
2022-01-10 16:24:47 -0800

    Added cumulative link model for ordinal logistic regression

--- Host_analyses/Feeding_behaviour/Feeding_analysis.R
+++ Host_analyses/Feeding_behaviour/Feeding_analysis.R
@@ -1,10 +1,10 @@
-#### Feeding behaviour analyses and figures ####
+#### Feeding behaviour analyses using an ordinal logistic regression####
 
 # libraries 
+library(reshape2)
 library(tidyverse)
 library(cowplot)
-library(ggpubr)
-library(reshape2)
+library(ordinal)
 
 # data loading and tidying 
 as_feeding = read.csv("Astrangia_files/Feeding_data.csv") %>%
@@ -32,82 +32,60 @@ oc_feeding = read.csv("Oculina_files/Oculina Feeding Behavior - Sheet1.csv") %>%
   group_by(polyp_behaviour, Day, Treatment, Sym.Status, Genotype, system) %>%
   dplyr::summarise(Frequency = n()) 
 
-#### Plot it ####
+#### Ordinal logistic regression treating genotype and system as random effects. ####
 
-#add colour pallet 
-#extracting hex codes from different pallets
-brewer.pal(10, "Blues")
-brewer.pal(10, "Greys")
-brewer.pal(5, "Reds")
+# Astrangia Full Model 
+as_feeding_data = as_feeding %>%
+  mutate(polyp_behaviour = factor(polyp_behaviour, levels = 1:5, ordered = T),
+         Treatment = as.factor(Treatment),
+         Treatment = relevel(Treatment, ref = "Control"),
+         Sym.Status = as.factor(Sym.Status),
+         Sym.Status = relevel(Sym.Status, ref = "White"),
+         Genotype = as.factor(Genotype), 
+         system = as.factor(system))
 
-as_color = as_feeding %>%
-  mutate(colors = case_when(
-    Treatment == "Heat" & polyp_behaviour == "1" ~ "#FEE5D9",
-    Treatment == "Heat" & polyp_behaviour == "2" ~ "#FCAE91",
-    Treatment == "Heat" & polyp_behaviour == "3" ~ "#FB6A4A",
-    Treatment == "Heat" & polyp_behaviour == "4" ~ "#DE2D26",
-    Treatment == "Heat" & polyp_behaviour == "5" ~ "#A50F15",
-    Treatment == "Control" & polyp_behaviour == "5" ~ "#525252",
-    Treatment == "Control" & polyp_behaviour == "4" ~ "#969696",
-    Treatment == "Control" & polyp_behaviour == "3" ~ "#BDBDBD",
-    Treatment == "Control" & polyp_behaviour == "2" ~ "#D9D9D9",
-    Treatment == "Control" & polyp_behaviour == "1" ~ "#F0F0F0",
-    Treatment == "Cold" & polyp_behaviour == "5" ~ "#08519C",
-    Treatment == "Cold" & polyp_behaviour == "4" ~ "#3182BD",
-    Treatment == "Cold" & polyp_behaviour == "3" ~ "#6BAED6",
-    Treatment == "Cold" & polyp_behaviour == "2" ~ "#BDD7E7",
-    Treatment == "Cold" & polyp_behaviour == "1" ~ "#C6DBEF",
-  ))
+as_model = clmm(polyp_behaviour ~ Treatment * Sym.Status + (1 | system) + (1 | Genotype), data = as_feeding_data)
+as_summary = summary(as_model) # note, object is saved for inspection at end of script
 
-as_color_codes <- as_color$colors
-names(as_color_codes) <- as_color$colors
+# Astrangia reduced model, no interaction
+as_reduced_model = clmm(polyp_behaviour ~ Treatment + Sym.Status + (1 | system) + (1 | Genotype), data = as_feeding_data)
+summary(as_reduced_model)
 
-oc_color = oc_feeding %>%
-  mutate(colors = case_when(
-    Treatment == "Heat" & polyp_behaviour == "5" ~ "#A50F15",
-    Treatment == "Heat" & polyp_behaviour == "4" ~ "#DE2D26",
-    Treatment == "Heat" & polyp_behaviour == "3" ~ "#FB6A4A",
-    Treatment == "Heat" & polyp_behaviour == "2" ~ "#FCAE91",
-    Treatment == "Heat" & polyp_behaviour == "1" ~ "#FEE5D9",
-    Treatment == "Control" & polyp_behaviour == "5" ~ "#525252",
-    Treatment == "Control" & polyp_behaviour == "4" ~ "#969696",
-    Treatment == "Control" & polyp_behaviour == "3" ~ "#BDBDBD",
-    Treatment == "Control" & polyp_behaviour == "2" ~ "#D9D9D9",
-    Treatment == "Control" & polyp_behaviour == "1" ~ "#F0F0F0",
-    Treatment == "Cold" & polyp_behaviour == "5" ~ "#08519C",
-    Treatment == "Cold" & polyp_behaviour == "4" ~ "#3182BD",
-    Treatment == "Cold" & polyp_behaviour == "3" ~ "#6BAED6",
-    Treatment == "Cold" & polyp_behaviour == "2" ~ "#BDD7E7",
-    Treatment == "Cold" & polyp_behaviour == "1" ~ "#C6DBEF",
-  ))
-oc_color_codes <- oc_color$colors
-names(oc_color_codes) <- oc_color$colors
+# Astrangia interaction by likelihood ratio test
+as_comparison = anova(as_model, as_reduced_model)
 
-as_behaviour_plot = 
-  ggplot(as_color, aes(y = Frequency, x = Day, fill = as.factor(colors))) + 
-  geom_bar(stat = "identity", position = position_fill(reverse = TRUE)) +
-  facet_grid(Treatment ~ Sym.Status) + 
-  labs(fill = "Behavioural Score") +
-  scale_fill_manual(values = as_color_codes) +
-  theme_cowplot()
+## Oculina full Model 
+oc_feeding_data = oc_feeding %>%
+  mutate(polyp_behaviour = factor(polyp_behaviour, levels = 1:5, ordered = T),
+         Treatment = as.factor(Treatment),
+         Treatment = relevel(Treatment, ref = "Control"),
+         Sym.Status = as.factor(Sym.Status),
+         Sym.Status = relevel(Sym.Status, ref = "White"),
+         Genotype = as.factor(Genotype), 
+         system = as.factor(system))
 
-oc_behaviour_plot = 
-  ggplot(oc_color, aes(y = Frequency, x = Day, fill = as.factor(colors))) + 
-  geom_bar(stat = "identity", position = position_fill(reverse = TRUE)) +
-  facet_grid(Treatment ~ Sym.Status) + 
-  labs(fill = "Behavioural Score") +
-  scale_fill_manual(values = oc_color_codes) +
-  theme_cowplot()
+oc_model = clmm(polyp_behaviour ~ Treatment * Sym.Status + (1 | system) + (1 | Genotype), data = oc_feeding_data)
+oc_summary = summary(oc_model)
 
-behaviour = ggarrange(as_behaviour_plot,
-                      oc_behaviour_plot,
-                      common.legend = TRUE,
-                      legend = "none",
-                      labels = c("A. Astrangia poculata", "B. Oculina arbuscula"),
-                      vjust = 0.5,
-                      hjust = -.25)
+# Oculina reduced model
+oc_reduced_model = clmm(polyp_behaviour ~ Treatment + Sym.Status + (1 | system) + (1 | Genotype), data = oc_feeding_data)
+summary(oc_reduced_model)
 
-ggsave("Behaviour.png", plot = behaviour, width = 8, height = 5, units = "in")
-ggsave("Behaviour.pdf", plot = behaviour, width = 8, height = 5, units = "in")
+# Oculina likelihood ratio test, comparing models
+oc_comparison = anova(oc_model, oc_reduced_model)
 
-# note, legend is added in illustrator as it is a pain to make in R
+
+# Write csv's of model results
+write.csv(as_summary$coefficients, "Astrangia_files/astrangia_coefficients.csv")
+write.csv(as_summary$info, "Astrangia_files/astrangia_info.csv")
+write.csv(as_comparison, "Astrangia_files/astrangia_likelihood_ratio.csv")
+
+write.csv(oc_summary$coefficients, "Oculina_files/oculina_coefficients.csv")
+write.csv(oc_summary$info, "Oculina_files/oculina_info.csv")
+write.csv(oc_comparison, "Oculina_files/oculina_likelihood_ratio.csv")
+
+
+
+
+
+

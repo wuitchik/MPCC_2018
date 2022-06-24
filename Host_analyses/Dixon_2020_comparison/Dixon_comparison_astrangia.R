48ad97777f2b43c828375e3e18f1d497eb7d2711
Daniel Wuitchik <Daniel.Wuitchik@gmail.com>
2022-06-24 09:47:30 -0700

    Dixon selected GO terms added

--- Host_analyses/Dixon_2020_comparison/Dixon_comparison_astrangia.R
+++ Host_analyses/Dixon_2020_comparison/Dixon_comparison_astrangia.R
@@ -9,13 +9,19 @@ Dixon_MF = read.table("Dixon_MF.csv", header = T)
 Dixon_CC = read.table("Dixon_CC.csv", header = T)
 
 # Read in Astrangia GO, Symbiont comparisons
-Ast_Heat_Sym_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
-Ast_Heat_Sym_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
-Ast_Heat_Sym_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_Sym_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_Sym_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_Sym_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
 
-Ast_Heat_apo_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
-Ast_Heat_apo_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
-Ast_Heat_apo_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_apo_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_apo_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_apo_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
+
+# Table of stress GO's 
+
+stress = read.csv("stress_list.csv") %>%
+  group_by(summary)
+
 
 
 #### Heat Treatment ####
@@ -23,14 +29,17 @@ Ast_Heat_apo_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_apo_control_vs_hot_res
 
 #### Brown Phenotype 
 # Biological Processes
-heat_sym_goods = intersect(Ast_Heat_Sym_BP$term, Dixon_BP$term)
-heat_sym = Ast_Heat_Sym_BP[Ast_Heat_Sym_BP$term %in% heat_sym_goods,]
+heat_sym_goods = intersect(Heat_Sym_BP$term, Dixon_BP$term)
+heat_sym = Heat_Sym_BP[Heat_Sym_BP$term %in% heat_sym_goods,]
 Dixon_BP_set = Dixon_BP[Dixon_BP$term %in% heat_sym_goods,]
 
 # Combine them
 heat_sym_BP_data = merge(heat_sym, Dixon_BP_set, by="term")
 
-heat_sym_BP_plot = ggplot(heat_sym_BP_data, aes(delta.rank.x, delta.rank.y, label = name.y)) +
+stress_data = heat_sym_BP_data[heat_sym_BP_data$term %in% stress$term,] %>%
+  left_join(stress)
+
+heat_sym_BP_plot = ggplot(heat_sym_BP_data, aes(delta.rank.x, delta.rank.y)) +
   scale_fill_distiller(palette = "Reds", direction = 1) +
   geom_hex() +
   labs( x = "Brown Phenotype",
@@ -38,11 +47,13 @@ heat_sym_BP_plot = ggplot(heat_sym_BP_data, aes(delta.rank.x, delta.rank.y, labe
   geom_vline(xintercept = 0, linetype = 2, alpha = 0.75) +
   geom_hline(yintercept = 0, linetype = 2, alpha = 0.75) +
   geom_smooth(method=lm, color="black", se =FALSE) +
+  geom_point(data = stress_data) + 
+  geom_text_repel(data = stress_data, aes(label = summary)) +
   theme_cowplot()
 
 # Molecular Function
-heat_sym_goods_MF = intersect(Ast_Heat_Sym_MF$term, Dixon_MF$term)
-heat_sym_MF = Ast_Heat_Sym_MF[Ast_Heat_Sym_MF$term %in% heat_sym_goods_MF,]
+heat_sym_goods_MF = intersect(Heat_Sym_MF$term, Dixon_MF$term)
+heat_sym_MF = Heat_Sym_MF[Heat_Sym_MF$term %in% heat_sym_goods_MF,]
 Dixon_MF_set = Dixon_MF[Dixon_MF$term %in% heat_sym_goods_MF,]
 
 # Combine them
@@ -59,8 +70,8 @@ heat_sym_MF_plot = ggplot(heat_sym_MF_data, aes(delta.rank.x, delta.rank.y, labe
   theme_cowplot()
 
 # Cellular Component
-heat_sym_goods_CC = intersect(Ast_Heat_Sym_CC$term, Dixon_CC$term)
-heat_sym_CC = Ast_Heat_Sym_CC[Ast_Heat_Sym_CC$term %in% heat_sym_goods_CC,]
+heat_sym_goods_CC = intersect(Heat_Sym_CC$term, Dixon_CC$term)
+heat_sym_CC = Heat_Sym_CC[Heat_Sym_CC$term %in% heat_sym_goods_CC,]
 Dixon_CC_set = Dixon_CC[Dixon_CC$term %in% heat_sym_goods_CC,]
 
 # Combine them
@@ -79,14 +90,18 @@ heat_sym_CC_plot = ggplot(heat_sym_CC_data, aes(delta.rank.x, delta.rank.y, labe
 
 #### White Phenotype 
 # Biological Processes
-heat_apo_goods = intersect(Ast_Heat_apo_BP$term, Dixon_BP$term)
-heat_apo = Ast_Heat_apo_BP[Ast_Heat_apo_BP$term %in% heat_apo_goods,]
+heat_apo_goods = intersect(Heat_apo_BP$term, Dixon_BP$term)
+heat_apo = Heat_apo_BP[Heat_apo_BP$term %in% heat_apo_goods,]
 Dixon_BP_set = Dixon_BP[Dixon_BP$term %in% heat_apo_goods,]
 
 # Combine them
 heat_apo_BP_data = merge(heat_apo, Dixon_BP_set, by="term")
 
-heat_apo_BP_plot = ggplot(heat_apo_BP_data, aes(delta.rank.x, delta.rank.y, label = name.y)) +
+heat_apo_stress_data = heat_apo_BP_data[heat_apo_BP_data$term %in% stress$term,] %>%
+  left_join(stress)
+
+
+heat_apo_BP_plot = ggplot(heat_apo_BP_data, aes(delta.rank.x, delta.rank.y)) +
   scale_fill_distiller(palette = "Reds", direction = 1) +
   geom_hex() +
   labs( x = "White Phenotype",
@@ -94,11 +109,14 @@ heat_apo_BP_plot = ggplot(heat_apo_BP_data, aes(delta.rank.x, delta.rank.y, labe
   geom_vline(xintercept = 0, linetype = 2, alpha = 0.75) +
   geom_hline(yintercept = 0, linetype = 2, alpha = 0.75) +
   geom_smooth(method=lm, color="black", se =FALSE) +
+  geom_point(data = heat_apo_stress_data) + 
+  geom_text_repel(data = stress_data, aes(label = summary)) +
   theme_cowplot()
 
+
 # Molecular Function
-heat_apo_goods_MF = intersect(Ast_Heat_apo_MF$term, Dixon_MF$term)
-heat_apo_MF = Ast_Heat_apo_MF[Ast_Heat_apo_MF$term %in% heat_apo_goods_MF,]
+heat_apo_goods_MF = intersect(Heat_apo_MF$term, Dixon_MF$term)
+heat_apo_MF = Heat_apo_MF[Heat_apo_MF$term %in% heat_apo_goods_MF,]
 Dixon_MF_set = Dixon_MF[Dixon_MF$term %in% heat_apo_goods_MF,]
 
 # Combine them
@@ -115,8 +133,8 @@ heat_apo_MF_plot = ggplot(heat_apo_MF_data, aes(delta.rank.x, delta.rank.y, labe
   theme_cowplot()
 
 # Cellular Component
-heat_apo_goods_CC = intersect(Ast_Heat_apo_CC$term, Dixon_CC$term)
-heat_apo_CC = Ast_Heat_apo_CC[Ast_Heat_apo_CC$term %in% heat_apo_goods_CC,]
+heat_apo_goods_CC = intersect(Heat_apo_CC$term, Dixon_CC$term)
+heat_apo_CC = Heat_apo_CC[Heat_apo_CC$term %in% heat_apo_goods_CC,]
 Dixon_CC_set = Dixon_CC[Dixon_CC$term %in% heat_apo_goods_CC,]
 
 # Combine them
@@ -136,26 +154,26 @@ heat_apo_CC_plot = ggplot(heat_apo_CC_data, aes(delta.rank.x, delta.rank.y, labe
  (heat_apo_BP_plot + heat_apo_MF_plot + heat_apo_CC_plot))
 
 ggsave("Astrangia_heat_within_sym.pdf", 
-       last_plot(),
+       lplot(),
        width = 13,
        height = 7,
        units = "in")
 
 #### Cold Treatment ####
 # Read in Astrangia GO, Symbiont comparisons
-Ast_Cold_Sym_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_sym_control_vs_cold_results_modified_pvalues.csv", header = T)
-Ast_Cold_Sym_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_sym_control_vs_cold_results_modified_pvalues.csv", header = T)
-Ast_Cold_Sym_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_sym_control_vs_cold_results_modified_pvalues.csv", header = T)
+Cold_Sym_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_sym_control_vs_cold_results_modified_pvalues.csv", header = T)
+Cold_Sym_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_sym_control_vs_cold_results_modified_pvalues.csv", header = T)
+Cold_Sym_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_sym_control_vs_cold_results_modified_pvalues.csv", header = T)
 
-Ast_Cold_apo_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_apo_control_vs_cold_results_modified_pvalues.csv", header = T)
-Ast_Cold_apo_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_apo_control_vs_cold_results_modified_pvalues.csv", header = T)
-Ast_Cold_apo_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_apo_control_vs_cold_results_modified_pvalues.csv", header = T)
+Cold_apo_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_apo_control_vs_cold_results_modified_pvalues.csv", header = T)
+Cold_apo_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_apo_control_vs_cold_results_modified_pvalues.csv", header = T)
+Cold_apo_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_apo_control_vs_cold_results_modified_pvalues.csv", header = T)
 
 #### Brown Phenotype 
 
 # Biological Processes
-Cold_sym_goods = intersect(Ast_Cold_Sym_BP$term, Dixon_BP$term)
-Cold_sym = Ast_Cold_Sym_BP[Ast_Cold_Sym_BP$term %in% Cold_sym_goods,]
+Cold_sym_goods = intersect(Cold_Sym_BP$term, Dixon_BP$term)
+Cold_sym = Cold_Sym_BP[Cold_Sym_BP$term %in% Cold_sym_goods,]
 Dixon_BP_set = Dixon_BP[Dixon_BP$term %in% Cold_sym_goods,]
 
 # Combine them
@@ -172,8 +190,8 @@ Cold_sym_BP_plot = ggplot(Cold_sym_BP_data, aes(delta.rank.x, delta.rank.y, labe
   theme_cowplot()
 
 # Molecular Function
-Cold_sym_goods_MF = intersect(Ast_Cold_Sym_MF$term, Dixon_MF$term)
-Cold_sym_MF = Ast_Cold_Sym_MF[Ast_Cold_Sym_MF$term %in% Cold_sym_goods_MF,]
+Cold_sym_goods_MF = intersect(Cold_Sym_MF$term, Dixon_MF$term)
+Cold_sym_MF = Cold_Sym_MF[Cold_Sym_MF$term %in% Cold_sym_goods_MF,]
 Dixon_MF_set = Dixon_MF[Dixon_MF$term %in% Cold_sym_goods_MF,]
 
 # Combine them
@@ -190,8 +208,8 @@ Cold_sym_MF_plot = ggplot(Cold_sym_MF_data, aes(delta.rank.x, delta.rank.y, labe
   theme_cowplot()
 
 # Cellular Component
-Cold_sym_goods_CC = intersect(Ast_Cold_Sym_CC$term, Dixon_CC$term)
-Cold_sym_CC = Ast_Cold_Sym_CC[Ast_Cold_Sym_CC$term %in% Cold_sym_goods_CC,]
+Cold_sym_goods_CC = intersect(Cold_Sym_CC$term, Dixon_CC$term)
+Cold_sym_CC = Cold_Sym_CC[Cold_Sym_CC$term %in% Cold_sym_goods_CC,]
 Dixon_CC_set = Dixon_CC[Dixon_CC$term %in% Cold_sym_goods_CC,]
 
 # Combine them
@@ -210,8 +228,8 @@ Cold_sym_CC_plot = ggplot(Cold_sym_CC_data, aes(delta.rank.x, delta.rank.y, labe
 #### White phenotype
 
 # Biological Processes
-Cold_apo_goods = intersect(Ast_Cold_apo_BP$term, Dixon_BP$term)
-Cold_apo = Ast_Cold_apo_BP[Ast_Cold_apo_BP$term %in% Cold_apo_goods,]
+Cold_apo_goods = intersect(Cold_apo_BP$term, Dixon_BP$term)
+Cold_apo = Cold_apo_BP[Cold_apo_BP$term %in% Cold_apo_goods,]
 Dixon_BP_set = Dixon_BP[Dixon_BP$term %in% Cold_apo_goods,]
 
 # Combine them
@@ -228,8 +246,8 @@ Cold_apo_BP_plot = ggplot(Cold_apo_BP_data, aes(delta.rank.x, delta.rank.y, labe
   theme_cowplot()
 
 # Molecular Function
-Cold_apo_goods_MF = intersect(Ast_Cold_apo_MF$term, Dixon_MF$term)
-Cold_apo_MF = Ast_Cold_apo_MF[Ast_Cold_apo_MF$term %in% Cold_apo_goods_MF,]
+Cold_apo_goods_MF = intersect(Cold_apo_MF$term, Dixon_MF$term)
+Cold_apo_MF = Cold_apo_MF[Cold_apo_MF$term %in% Cold_apo_goods_MF,]
 Dixon_MF_set = Dixon_MF[Dixon_MF$term %in% Cold_apo_goods_MF,]
 
 # Combine them
@@ -246,8 +264,8 @@ Cold_apo_MF_plot = ggplot(Cold_apo_MF_data, aes(delta.rank.x, delta.rank.y, labe
   theme_cowplot()
 
 # Cellular Component
-Cold_apo_goods_CC = intersect(Ast_Cold_apo_CC$term, Dixon_CC$term)
-Cold_apo_CC = Ast_Cold_apo_CC[Ast_Cold_apo_CC$term %in% Cold_apo_goods_CC,]
+Cold_apo_goods_CC = intersect(Cold_apo_CC$term, Dixon_CC$term)
+Cold_apo_CC = Cold_apo_CC[Cold_apo_CC$term %in% Cold_apo_goods_CC,]
 Dixon_CC_set = Dixon_CC[Dixon_CC$term %in% Cold_apo_goods_CC,]
 
 # Combine them
@@ -267,7 +285,7 @@ Cold_apo_CC_plot = ggplot(Cold_apo_CC_data, aes(delta.rank.x, delta.rank.y, labe
     (Cold_apo_BP_plot + Cold_apo_MF_plot + Cold_apo_CC_plot))
 
 ggsave("Astrangia_cold_within_sym.pdf", 
-       last_plot(),
+       lplot(),
        width = 13,
        height = 7,
        units = "in")

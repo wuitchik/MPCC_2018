1c711ba7a41de519ceea87c5a32d4fafd4d34c19
Daniel Wuitchik <Daniel.Wuitchik@gmail.com>
2022-08-01 14:11:51 -0700

    Parent GO

--- Host_analyses/lolipops/lolipop.R
+++ Host_analyses/lolipops/lolipop.R
@@ -1,7 +1,7 @@
 # lolipop script
 
 library(tidyverse)
-l
+library(GOfuncR)
 
 # first trying to get to parent GO terms
 Heat_Sym_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
@@ -11,3 +11,9 @@ Heat_Sym_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_sym_control_vs_hot_results
 Heat_apo_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
 Heat_apo_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
 Heat_apo_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
+
+# testing
+
+names = Heat_Sym_BP$term
+
+get_parent_nodes(names)

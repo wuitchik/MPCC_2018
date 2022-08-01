adfb47d58c37480c4837589e08bd08a1f6f01638
Daniel Wuitchik <Daniel.Wuitchik@gmail.com>
2022-08-01 13:48:49 -0700

    starterd lolipop analysis

---
+++ Host_analyses/lolipops/lolipop.R
@@ -0,0 +1,13 @@
+# lolipop script
+
+library(tidyverse)
+l
+
+# first trying to get to parent GO terms
+Heat_Sym_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_Sym_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_Sym_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_sym_control_vs_hot_results_modified_pvalues.csv", header = T)
+
+Heat_apo_BP = read.table("../GO_DEGs/Astrangia/MWU_BP_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_apo_MF = read.table("../GO_DEGs/Astrangia/MWU_MF_apo_control_vs_hot_results_modified_pvalues.csv", header = T)
+Heat_apo_CC = read.table("../GO_DEGs/Astrangia/MWU_CC_apo_control_vs_hot_results_modified_pvalues.csv", header = T)

e6973298edf5e8b3fc3c355d1fb61eda57693723
hannahaichelman <hannahaichelman@gmail.com>
2022-11-29 10:34:32 -0500

    Update SNP analysis.

--- Sym_analyses/SNPCalling/Oculina/Cluster_Dendrogram_plot.R
+++ Sym_analyses/SNPCalling/Oculina/Cluster_Dendrogram_plot.R
@@ -1,5 +1,5 @@
 
-setwd("/Users/hannahaichelman/Documents/BU/MPCC_TagSeq/SNPcalling/Oculina")
+setwd("/Users/hannahaichelman/Documents/BU/Host_Buffering/MPCC_2018/Sym_analyses/SNPCalling/Oculina")
 #clustering / PCoA based on identity by state (IBS) 
 #(for low and/or uneven coverage)
 
@@ -13,5 +13,6 @@ ma=ma[goods,goods]
 dimnames(ma)=list(bams[goods],bams[goods])
 hc=hclust(as.dist(ma),"ave")
 plot(hc,cex=0.7) 
-abline(h=0.17, col='darkgray', lty = 2, lwd=2) # add a vertical line
-#James uses cut-off of 0.15, so any samples that fall below that threshold he calls a clone
+abline(h=0.17, col='darkgray', lty = 2, lwd=2) # add a vertical line to indicate cut-off for clones
+# This cut-off aligns well with what James Fifer has used in the past (0.15), 
+# this means that any samples that cluster together below that threshold are clones

c0bf87c6dd8a2480fe85b4946e76b8155d51df9e
Daniel Wuitchik <Daniel.Wuitchik@gmail.com>
2023-03-22 13:20:03 -0400

    Colour analyses

---
+++ Host_analyses/Colour_analysis/colour_analysis.R
@@ -0,0 +1,61 @@
+# Colour analysis
+library(tidyverse)
+library(performance)
+
+# load data
+colour_data = read.csv("astrangia_relative_counts.csv")
+load("../DESeq2/Astrangia/sym_dds.RData")
+
+# add meta data to colour spreadsheet
+data = colour_data %>%
+  right_join(expDesign) %>%
+  rename(Phenotype = Sym.Status) %>%
+  select(-System, -Polyp_behaviour, -Day, -Genotype, -Treatment_by_Sym.Status)
+
+# make sym count to host count ratio
+data = data %>%
+  mutate(Sym.Percent = ((Sym / Holobiont)*100))
+
+# model selection
+gaussian = glm(Sym.Percent ~ Treatment + Phenotype + Treatment:Phenotype, family = gaussian, data = data)
+check_normality(gaussian) # Warning: Non-normality of residuals detected (p = 0.037).
+check_heteroscedasticity(gaussian) # OK: Error variance appears to be homoscedastic (p = 0.152)
+
+gaussian_sqrt_transform = glm(sqrt(Sym.Percent) ~ Treatment + Phenotype + Treatment:Phenotype, data = data)
+check_normality(gaussian_sqrt_transform) # OK: residuals appear as normally distributed (p = 0.197).
+check_heteroscedasticity(gaussian_sqrt_transform) # OK: Error variance appears to be homoscedastic (p = 0.934).
+check_model(gaussian_sqrt_transform)
+summary(gaussian_sqrt_transform)
+
+# all:
+#   glm(formula = sqrt(Sym.Percent) ~ Treatment + Phenotype + Treatment:Phenotype, 
+#       data = data)
+# 
+# Deviance Residuals: 
+#   Min       1Q   Median       3Q      Max  
+# -1.0204  -0.2849  -0.1197   0.4092   1.0876  
+# 
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)                      1.60767    0.17050   9.429  6.9e-11 ***
+#   TreatmentControl                -0.22681    0.27492  -0.825 0.415283    
+# TreatmentHeat                    0.02881    0.24958   0.115 0.908805    
+# PhenotypeWhite                  -0.96638    0.24958  -3.872 0.000483 ***
+#   TreatmentControl:PhenotypeWhite  0.45915    0.37131   1.237 0.224976    
+# TreatmentHeat:PhenotypeWhite     0.37697    0.39198   0.962 0.343196    
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# (Dispersion parameter for gaussian family taken to be 0.2325512)
+# 
+# Null deviance: 13.6315  on 38  degrees of freedom
+# Residual deviance:  7.6742  on 33  degrees of freedom
+# AIC: 61.275
+# 
+# Number of Fisher Scoring iterations: 2
+
+report(gaussian_sqrt_transform)
+
+# plot it
+
+plot = ggplot()

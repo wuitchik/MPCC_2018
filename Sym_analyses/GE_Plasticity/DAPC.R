acb45f25f95e80e00020ab41bba0fdec077cf108
hannahaichelman <hannahaichelman@gmail.com>
2023-08-01 12:19:01 -0400

    Update DAPCs.

--- Sym_analyses/GE_Plasticity/DAPC.R
+++ Sym_analyses/GE_Plasticity/DAPC.R
@@ -1,4 +1,7 @@
 library(adegenet)
+library(tidyverse)
+library(plyr)
+library(DESeq2)
 
 
 #### Host Data ####
@@ -31,8 +34,9 @@ symState  = as.factor(sapply(strsplit(colnames(countDataHost), split = "_"), "[[
 
 expDesign_Host = data.frame(colnames(countDataHost), treatmentHost, genotypeHost, symState)
 expDesign_Host$type = "host"
-expDesign_Host$treat_type = paste(expDesign_Host$treatmentHost,expDesign_Host$symState, sep = "_")
+expDesign_Host$treat_type = as.factor(paste(expDesign_Host$treatmentHost,expDesign_Host$symState, sep = "_"))
 names(expDesign_Host) = c("sample", "treatment", "genotype", "sym_state", "type", "treat_type")
+str(expDesign_Host)
 
 colSums(countDataHost)
 
@@ -53,46 +57,41 @@ dds = DESeq(dds)
 
 # rlog transformation
 rlogged = rlog(dds, blind = TRUE)
-vst = vst(dds, blind=TRUE)
-
 
 dat_rlog = as.data.frame(assay(rlogged))
-dat_vst = as.data.frame(assay(vst))
 
 # Now run DAPC - look for optimal number of clusters
-clus = find.clusters(t(dat_rlog), max.n.clust=30) # 20 pc's and 2 clusters
+clus = find.clusters(t(dat_rlog), max.n.clust=30) # 20 pc's and 6 clusters
 names(clus)
-clus$grp
-table(pop(dat_rlog), clus$grp)
+clus$grp = expDesign_Host_clones_removed$treat_type
+#head(clus$grp, 6)
+#clus$size
+# 5 3 5 6 5 9
 
-# freezing corals all in one group, heat and control in the other. lines up well with what we see in the pca 
-
-dapc1 = dapc(t(dat_rlog), clus$grp)
-
-scatter(dapc1, bg = "white", legend = TRUE, solid = .4)
+# use a-score to find the optimal number of PCs
+dapc1 = dapc(t(dat_rlog), n.da = 100, n.pca = 20, clus$grp)
+temp = optim.a.score(dapc1) # optimal number of PCs = 10
 
 
+# colours 
 
-## convert vsd to matrix
-rlog.corr.ge.matrix <- t(assay(rlogged.corr)) 
+myCol = c("#9ecae1", "#3182bd","lightgrey","grey","#fc9272", "#de2d26")
 
-## grouping variable:
-grp_pca <- DESeq2::plotPCA(rlogged.corr, intgroup = c("treat_type", "type"), returnData = TRUE)
+shapes = c(21, 19, 21, 19, 21, 19)
 
-## 
-dapc1 = dapc(rlog.corr.ge.matrix, grp_pca$treat_type)
-scatter(dapc1)
-scatter(dapc1,1,1)
+# now lets build a discriminant function for these six groups:
+dp=dapc(t(dat_rlog),clus$grp) # 2pcs, 6 discriminant functions
 
-find.clusters(rlog.corr.ge.matrix, max.n.clust=40)
+pdf("/Users/hannahaichelman/Dropbox/BU/Host_Buffering/MPCC_2018/Sym_analyses/GE_Plasticity/dapc_2PC_6DF_host.pdf")
+scatter(dp,1,1, bg = "white", legend = TRUE, posi.leg = "topleft", col = myCol, pch = shapes, solid = 0.8)
+dev.off()
 
-## Run the treatment and type DAPC with 43 PCs and 6 DAs
-dapc2 <- dapc(rlog.corr.ge.matrix, grp_pca$treat_type, n.pca = 3, n.da = 6)
-dapc2
-summary(dapc2)
-scatter(dapc2)
 
-scatter(dapc2,1,1, cols = cols, bg = "white", scree.da = FALSE, legend = TRUE, solid = 0.6)
+pdf("/Users/hannahaichelman/Dropbox/BU/Host_Buffering/MPCC_2018/Sym_analyses/GE_Plasticity/dapc_pca_2PC_6DF_host.pdf")
+scatter(dp, bg = "white", scree.da = TRUE, legend = TRUE, posi.leg = "topleft",
+        solid = 0.8, col = myCol, scree.pca = TRUE,
+        clabel = 0)
+dev.off()
 
 #### Symbiont in Host Data ####
 
@@ -129,28 +128,95 @@ genotypeSym  = as.factor(sapply(strsplit(colnames(countDataSym_brown_noclone), s
 
 expDesign_Sym = data.frame(colnames(countDataSym_brown_noclone), treatmentSym, genotypeSym)
 expDesign_Sym$type = "sym_inhost"
-expDesign_Sym$treat_type = paste(expDesign_Sym$treatmentSym,expDesign_Sym$type, sep = "_")
+expDesign_Sym$treat_type = as.factor(paste(expDesign_Sym$treatmentSym,expDesign_Sym$type, sep = "_"))
 names(expDesign_Sym) = c("sample", "treatment", "genotype", "type", "treat_type")
+str(expDesign_Sym)
 
 dds_Sym<-DESeqDataSetFromMatrix(countData=countDataSym_brown_noclone, colData=expDesign_Sym, design=~treatment) #can only test for the main effects of treatment
 dds_Sym = DESeq(dds_Sym)
 
 # rlog transformation
-rlogged_syminhost = rlog(dds, blind = TRUE)
-vst_syminhost = varianceStabilizingTransformation(dds, blind=TRUE)
+rlog_syminhost = rlog(dds_Sym, blind = TRUE)
 
-
-dat_rlog_syminhost = as.data.frame(assay(rlogged_syminhost))
-dat_vst_syminhost = as.data.frame(assay(vst_syminhost))
+dat_rlog_syminhost = as.data.frame(assay(rlog_syminhost))
 
 # Now run DAPC - look for optimal number of clusters
-clus_syminhost = find.clusters(t(dat_rlog_syminhost), max.n.clust=20) # 20 pc's and 2 clusters
+clus_syminhost = find.clusters(t(dat_rlog_syminhost), max.n.clust=20) # 15 pc's and 3 clusters
 names(clus_syminhost)
-clus_syminhost$grp
-# freezing corals all in one group, heat and control in the other. lines up well with what we see in the pca 
+clus_syminhost$grp = expDesign_Sym$treatment
 
-dapc1 = dapc(t(dat_rlog_syminhost), clus_syminhost$grp)
+# use a-score to find the optimal number of PCs
+dapc1_syminhost = dapc(t(dat_rlog_syminhost), n.da = 100, n.pca = 20, clus_syminhost$grp)
+temp_syminhost = optim.a.score(dapc1_syminhost) # optimal number of PCs = 1
 
-scatter(dapc1, bg = "white", scree.da = FALSE, legend = TRUE, solid = .4)
+
+# colours 
+
+cols_sym = c("control" = "#a6611a", "cold" = "#74c476", "heat" = "#fd8d3c")
+
+# now lets build a discriminant function for these six groups:
+dp_syminhost=dapc(t(dat_rlog_syminhost),clus_syminhost$grp) # 2pcs, 3 discriminant functions
+
+pdf("/Users/hannahaichelman/Dropbox/BU/Host_Buffering/MPCC_2018/Sym_analyses/GE_Plasticity/dapc_2PC_3DF_syminhost.pdf")
+scatter(dp_syminhost,1,1, bg = "white", legend = TRUE, posi.leg = "topright", col = cols_sym, solid = 0.8)
+dev.off()
+
+
+pdf("/Users/hannahaichelman/Dropbox/BU/Host_Buffering/MPCC_2018/Sym_analyses/GE_Plasticity/dapc_pca_2PC_3DF_syminhost.pdf")
+scatter(dp_syminhost, bg = "white", scree.da = TRUE, legend = TRUE, posi.leg = "topleft",
+        solid = 0.8, col = cols_sym, scree.pca = TRUE,
+        clabel = 0)
+dev.off()
+
+
+#### Symbiont in Culture Data ####
+
+countDataCulture <- read.table("/Users/hannahaichelman/Dropbox/BU/Host_Buffering/MPCC_2018/Sym_analyses/CountsFiles/B_psygmophilum_counts.txt")
+
+names(countDataCulture)=c( "Control_1.1", "Control_1", "Control_2.1", "Control_2", "Control_3.1", "Control_3", "Control_4.1", "Control_4", "Cool_1", "Cool_2", "Cool_3", "Cool_4","Heat_1.1", "Heat_1", "Heat_2.1", "Heat_2", "Heat_3.1", "Heat_3", "Heat_4.1", "Heat_4")
+row.names(countDataCulture)=sub("", "isogroup", rownames(countDataCulture))
+head(countDataCulture)
+
+treatment = as.factor(sapply(strsplit(colnames(countDataCulture), split = "_"), "[[", 1)) %>%
+  revalue(c("Control" = "control", "Cool" = "cold", "Heat" = "heat"))
+
+expDesignCulture = data.frame(treatment) 
+expDesignCulture$sample = colnames(countDataCulture)
+
+str(expDesignCulture)
+dds_Culture<-DESeqDataSetFromMatrix(countData=countDataCulture, colData=expDesignCulture, design=~treatment) #can only test for the main effects of treatment
+dds_Culture = DESeq(dds_Culture)
+
+rlog_culture = rlogTransformation(dds_Culture, blind = TRUE)
+
+dat_rlog_culture = as.data.frame(assay(rlog_culture))
+
+# Now run DAPC - look for optimal number of clusters
+clus_culture = find.clusters(t(dat_rlog_culture), max.n.clust=15) # 15 pc's and 3 clusters
+names(clus_culture)
+clus_culture$grp = expDesignCulture$treatment
+
+# use a-score to find the optimal number of PCs
+dapc1_culture = dapc(t(dat_rlog_culture), n.da = 100, n.pca = 15, clus_culture$grp)
+temp_culture = optim.a.score(dapc1_culture) # optimal number of PCs = 1
+
+
+# colours 
+
+cols_culture = c("control" = "#a6611a", "cold" = "#74c476", "heat" = "#fd8d3c")
+
+# now lets build a discriminant function for these six groups:
+dp_culture=dapc(t(dat_rlog_culture),clus_culture$grp) # 2pcs, 3 discriminant functions
+
+pdf("/Users/hannahaichelman/Dropbox/BU/Host_Buffering/MPCC_2018/Sym_analyses/GE_Plasticity/dapc_2PC_3DF_syminculture.pdf")
+scatter(dp_culture,1,1, bg = "white", legend = TRUE, posi.leg = "top", col = cols_culture, solid = 0.8)
+dev.off()
+
+
+pdf("/Users/hannahaichelman/Dropbox/BU/Host_Buffering/MPCC_2018/Sym_analyses/GE_Plasticity/dapc_pca_2PC_3DF_syminculture.pdf")
+scatter(dp_culture, bg = "white", scree.da = TRUE, legend = TRUE, posi.leg = "topleft",
+        solid = 0.8, col = cols_culture, scree.pca = TRUE,
+        clabel = 0)
+dev.off()
 
 
